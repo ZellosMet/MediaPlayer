6ab96f5cac0387182ba0e9d5d55203eff7a3d3cc
ZellosMet <zellossmet@gmail.com>
2023-11-21 22:01:11 +0900

    MediaPlayer V0.2

--- widget.cpp
+++ widget.cpp
@@ -3,6 +3,7 @@
 #include <QStyle>
 #include <QFileDialog>
 #include <QDir>
+#include <QTime>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -19,13 +20,19 @@ Widget::Widget(QWidget *parent)
     ui->btnPause->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
     ui->btnStop->setIcon(style()->standardIcon(QStyle::SP_MediaStop));
     ui->btnNext->setIcon(style()->standardIcon(QStyle::SP_MediaSkipForward));
+    ui->btnMute->setIcon(style()->standardIcon(QStyle::SP_MediaVolume));
+
+    ui->lComposition->setFrameStyle(QFrame::Box);
 
     //                      Player init
 
     m_player = new QMediaPlayer(this);
     m_player->setVolume(70);
-    ui->lVolume->setText(QString("Volume: ").append(QString::number(m_player->volume())));
+    ui->lVolume->setText(QString("Volume:").append(QString::number(m_player->volume())));
+    ui->lDuration->setText("00:00");
     ui->hsVolume->setValue(m_player->volume());
+    mute = false;
+    connect(m_player, &QMediaPlayer::positionChanged, this, &Widget::positionChanged);
 }
 
 Widget::~Widget()
@@ -42,7 +49,8 @@ void Widget::on_btnOpen_clicked()
                                                 "C:\\Users\\Zello\\OneDrive\\Рабочий стол\\Музыка",       //рабочий каталог
                                                 tr("Audio files (*.mp3 *.flac)")                          //выбор форматов файла
                                               );
-   ui->lComposition -> setText(file);
+   QStringList soung_name = file.split('/');
+   ui->lComposition -> setText(soung_name.last());
    m_player->setMedia(QUrl::fromLocalFile(file));
 }
 
@@ -57,5 +65,45 @@ void Widget::on_hsVolume_valueChanged(int value)
 void Widget::on_btnPlay_clicked()
 {
     m_player->play();
+    ui->lDuration->setText(QString(QTime::fromMSecsSinceStartOfDay(m_player->duration()).toString("mm:ss")));
+}
+
+void Widget::positionChanged(qint64 position)
+{
+    ui->hsProgress->setMaximum(m_player->duration());
+    ui->hsProgress->setValue(position);
+    ui->lProgress->setText(QString(QTime::fromMSecsSinceStartOfDay(position).toString("mm:ss")));
+}
+
+void Widget::on_hsProgress_sliderMoved(int position)
+{
+    m_player->setPosition(position);
+}
+
+void Widget::on_btnPause_clicked()
+{
+    m_player->pause();
+}
+
+void Widget::on_btnStop_clicked()
+{
+    m_player->stop();
+}
+
+
+void Widget::on_btnMute_clicked()
+{
+    if(!mute)
+    {
+        mute = !mute;
+        ui->btnMute->setIcon(style()->standardIcon(QStyle::SP_MediaVolumeMuted));
+        m_player->setMuted(mute);
+    }
+    else
+    {
+        mute = !mute;
+        ui->btnMute->setIcon(style()->standardIcon(QStyle::SP_MediaVolume));
+        m_player->setMuted(mute);
+    }
 }
 
--- widget.h
+++ widget.h
@@ -18,13 +18,17 @@ public:
 
 private slots:
     void on_btnOpen_clicked();
-
     void on_hsVolume_valueChanged(int value);
-
     void on_btnPlay_clicked();
+    void positionChanged(qint64 position);
+    void on_hsProgress_sliderMoved(int position);
+    void on_btnPause_clicked();
+    void on_btnStop_clicked();
+    void on_btnMute_clicked();
 
 private:
     Ui::Widget *ui;
     QMediaPlayer* m_player;
+    bool mute;
 };
 #endif // WIDGET_H
